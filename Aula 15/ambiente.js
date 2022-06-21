9dfe8eafb34542381208acfb7c573b3ef6c6a577
JeffChien <jeff.civil2@gmail.com>
2022-06-21 18:52:31 -0300

    Variaveis multiplas

---
+++ Aula 15/ambiente.js
@@ -0,0 +1,9 @@
+let num = [5, 8, 2, 9, 3]
+
+// num.sort() //colocar os números dentro do vetor em ordem crescente
+num.push(1) // adicionar o número em parenteses na última posição do vetor
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[1]}`)
+var pos = num.indexOf(8)
+console.log(`O valor 8 está na posição ${pos}`)
