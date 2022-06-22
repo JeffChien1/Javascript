073bf5be3ad28b7f2d0bd4e6a3587f388eaac82d
JeffChien <jeff.civil2@gmail.com>
2022-06-22 17:39:18 -0300

    Organizando as pastas 2

--- Aula 14ex/script.js
+++ Aula 14ex/script.js
@@ -1,1 +1,22 @@
-alert('Olá!')
+function carregar () {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+
+    msg.innerHTML = `Agora são ${hora} horas. `
+    if (hora>= 0 && hora < 12) {
+        //BOM DIA!
+        img.src = "foto-manha.jpg"
+        document.body.style.background = '#e0f1f8'
+    }else if (hora >= 12 && hora < 18) {
+        //BOA TARDE!
+        img.src = "foto-tarde.jpg"
+        document.body.style.background = '#e07b2b'
+    }else {
+        //BOA NOITE!
+        img.src = "foto-noite.jpg"
+        document.body.style.background = '#346dc7'
+    }
+}
+
